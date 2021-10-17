bc8bfbfab1f8bbc49c6234ecf26925525d9c5984
itsreallydrew <a.mclean2526@gmail.com>
2021-10-16 19:49:24 -0500

    Media query to hide logo at certain width

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import "./App.css";
-import Stocks from "./Stocks/Stocks";
-import StockSymbol from "./StockSymbol/StockSymbol";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
-import React, { useState } from "react";
-import About from "./About/About";
-import logo from "../src/assets/ssa-logo.png";
-import { Redirect } from "react-router-dom";
+import './App.css';
+import Stocks from './Stocks/Stocks';
+import StockSymbol from './StockSymbol/StockSymbol';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import About from './About/About';
+import logo from '../src/assets/ssa-logo.png';
+import { Redirect } from 'react-router-dom';
 
 function App() {
-  const [symbol, setSymbol] = useState([]);
+	const [symbol, setSymbol] = useState([]);
 
-  return (
-    <div>
-      <header>
-        <nav>
-          <Link to="/stocks">
-            <h1>Home</h1>
-          </Link>
-          <Link to="/stocks">
-            <img src={logo} alt="superior stock analysis" />
-          </Link>
+	return (
+		<div>
+			<header>
+				<nav>
+					<Link to="/stocks">
+						<h1>Home</h1>
+					</Link>
+					<Link to="/stocks">
+						<img src={logo} alt="superior stock analysis" id="logo" />
+					</Link>
 
-          <Link to="/about">
-            <h1>About</h1>
-          </Link>
-        </nav>
-      </header>
-      <main>
-        <Route exact path="/">
-          <Redirect to="/stocks" />
-        </Route>
-        <Route path="/stocks" component={Stocks} exact />
-        <Route path="/stocks/:symbol" component={StockSymbol} />
-        <Route path="/about" component={About} />
-      </main>
-    </div>
-  );
+					<Link to="/about">
+						<h1>About</h1>
+					</Link>
+				</nav>
+			</header>
+			<main>
+				<Route exact path="/">
+					<Redirect to="/stocks" />
+				</Route>
+				<Route path="/stocks" component={Stocks} exact />
+				<Route path="/stocks/:symbol" component={StockSymbol} />
+				<Route path="/about" component={About} />
+			</main>
+		</div>
+	);
 }
 
 export default App;
